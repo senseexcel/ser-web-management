dead8150fb632f58a9fcf0cb8f80b268c094995f
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2018-11-28 14:03:30 +0100

    change base page layout for ser-monitor, implement license file exists to check license file exists in content lib and exists logical

--- src/app/modules/ser-monitoring/pages/monitoring/monitoring-page.component.ts
+++ src/app/modules/ser-monitoring/pages/monitoring/monitoring-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LicenseValidator } from '@app/modules/ser-license/services';
+import { ILicenseValidationResult } from '@app/modules/ser-license/api/validation-result.interface';
+import { finalize, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-monitoring-page',
@@ -6,13 +10,65 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'monitoring-page.component.html',
 })
 
-export class MonitoringPageComponent implements OnInit {
+export class MonitoringPageComponent implements OnDestroy, OnInit {
 
+    /**
+     * data has been loaded, page is ready
+     *
+     * @type {boolean}
+     * @memberof MonitoringPageComponent
+     */
     public ready: boolean;
 
-    constructor() { }
+    /**
+     * determine we have an error for license (not exists, no rights)
+     *
+     * @type {boolean}
+     * @memberof MonitoringPageComponent
+     */
+    public hasError: boolean;
 
-    ngOnInit() {
-        this.ready = true;
+    /**
+     * license validation service
+     *
+     * @private
+     * @type {LicenseValidator}
+     * @memberof MonitoringPageComponent
+     */
+    private licenseValidator: LicenseValidator;
+
+    /**
+     * will submit true if component gets destroyed to unsubscribe
+     * from observables
+     *
+     * @private
+     * @type {Subject<boolean>}
+     * @memberof MonitoringPageComponent
+     */
+    private isDestroyed$: Subject<boolean>;
+
+    constructor(
+        validator: LicenseValidator
+    ) {
+        this.licenseValidator = validator;
+        this.isDestroyed$ = new Subject();
+        this.hasError = false;
+    }
+
+    public ngOnInit() {
+        this.licenseValidator.validateLicenseExists()
+            .pipe(
+                finalize(() => this.ready = true),
+                takeUntil(this.isDestroyed$)
+            )
+            .subscribe((result: ILicenseValidationResult) => {
+                if (!result.isValid) {
+                    this.hasError = true;
+                }
+            });
+    }
+
+    public ngOnDestroy() {
+        this.isDestroyed$.next(true);
     }
 }
